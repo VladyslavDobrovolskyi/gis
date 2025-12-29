7548724e047954b599f88182c00a974b8bdab762
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2025-12-29 21:58:45 +0200

    @gis/feat: Add sql queries for retrieving cities and countries with geometries

---
+++ apps/backend/db/sql/geo_objects.sql
@@ -0,0 +1,13 @@
+/* @name GetAllCitiesWithGeom */
+SELECT o.id, o.name, c.population, ST_AsText(p.geom) AS geometry
+FROM geo_object o
+JOIN city c ON c.geo_object_id = o.id
+LEFT JOIN geo_point p ON p.geo_object_id = o.id
+WHERE o.type_id = (SELECT id FROM geo_object_type WHERE code = 'CITY');
+
+/* @name GetAllCountriesWithGeom */
+SELECT o.id, o.name, co.iso_code, ST_AsText(mp.geom) AS geometry
+FROM geo_object o
+JOIN country co ON co.geo_object_id = o.id
+LEFT JOIN geo_multipolygon mp ON mp.geo_object_id = o.id
+WHERE o.type_id = (SELECT id FROM geo_object_type WHERE code = 'COUNTRY');
