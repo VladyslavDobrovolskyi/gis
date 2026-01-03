49113aafef5122531269d2ee9dd6e00b28600416
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2026-01-03 21:50:03 +0200

    @frontend/refactor: Clean up main.ts by removing unused imports and consolidating pinia store initialization

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,24 +1,16 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import { VueQueryPlugin } from '@tanstack/vue-query';
 import App from './App.vue';
+import { createApp } from 'vue';
+import { createPinia as PiniaStorePlugin } from 'pinia';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import '@/styles/map.styles.css';
 
-import './style.css';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet.markercluster/dist/leaflet.markercluster.js';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
-L.Icon.Default.mergeOptions({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-});
-const pinia = createPinia();
-
 const app = createApp(App);
-app.use(pinia);
+
 app.use(VueQueryPlugin);
+app.use(PiniaStorePlugin());
 
 app.mount('#app');
