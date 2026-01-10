4405b47df2733ab222321d2a41f88df306105514
Vladyslav Dobrovolskyi <vladyslav.dobrovolski@gmail.com>
2026-01-10 11:41:01 +0200

    @shared/feat: Add types for cities, countries, and regions

---
+++ packages/shared/types/cities.types.ts
@@ -0,0 +1,5 @@
+import { z } from 'zod';
+import { CitiesSchema, CitySchema } from '@schemas/cities.schema';
+
+export type City = z.infer<typeof CitySchema>;
+export type Cities = z.infer<typeof CitiesSchema>;
